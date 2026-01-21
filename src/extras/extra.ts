f0d9ac266801a5c0f8636d5af49fee838f71822e
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2026-01-21 02:10:46 -0500

    Refactor .gitignore handling in extra.ts by replacing synchronous file operations with asynchronous ones, improving performance and ensuring proper error handling during updates.

--- src/extras/extra.ts
+++ src/extras/extra.ts
@@ -1,5 +1,5 @@
-import { appendFile, existsSync, readFileSync } from "node:fs";
-import { writeFile } from "node:fs/promises";
+import { existsSync } from "node:fs";
+import { appendFile, readFile, writeFile } from "node:fs/promises";
 import path from "node:path";
 import type { spinner } from "@clack/prompts";
 
@@ -18,23 +18,21 @@ export const createEnvrc = async (spin: ReturnType<typeof spinner>) => {
 export const updateGitignore = async (spin: ReturnType<typeof spinner>) => {
   const gitignorePath = path.join(process.cwd(), ".gitignore");
   const exists = existsSync(gitignorePath);
+
   if (exists) {
-    const gitignoreContents = readFileSync(gitignorePath, "utf-8");
+    const gitignoreContents = await readFile(gitignorePath, "utf8");
     if (gitignoreContents.includes(".direnv")) {
       return;
     }
-  }
-  if (exists) {
+
     spin.start("Updating .gitignore");
-    appendFile(gitignorePath, "\n.direnv\n", (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-    });
-  } else {
-    spin.start("Creating .gitignore");
-    await writeFile(gitignorePath, ".direnv\n", "utf8");
+    await appendFile(gitignorePath, "\n.direnv\n", "utf8");
+    spin.stop(".gitignore updated");
+
+    return;
   }
+
+  spin.start("Creating .gitignore");
+  await writeFile(gitignorePath, ".direnv\n", "utf8");
   spin.stop(".gitignore updated");
 };
